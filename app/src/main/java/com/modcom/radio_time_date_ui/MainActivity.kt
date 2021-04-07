f263184cfa2f3e7cb891c252c0849cb83ada0af9
modcomlearning <modcomlearning@gmail.com>
2021-04-07 10:40:12 +0300

    First Commit

---
+++ app/src/main/java/com/modcom/radio_time_date_ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.modcom.radio_time_date_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //radio Buttons, date and time picker
+    }
+}
