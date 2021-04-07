dd9d388f3491b0aaea7dddc7ad589cdb240f677e
modcomlearning <modcomlearning@gmail.com>
2021-04-07 11:25:29 +0300

    First Commit

--- app/src/main/java/com/modcom/radio_time_date_ui/MainActivity.kt
+++ app/src/main/java/com/modcom/radio_time_date_ui/MainActivity.kt
@@ -2,11 +2,26 @@ package com.modcom.radio_time_date_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //radio Buttons, date and time picker
+
+        buttonbooking.setOnClickListener{
+            //which radio button was selected
+            val radio: RadioButton = findViewById(radiogroup.checkedRadioButtonId)
+            Toast.makeText(applicationContext, "You selected  ${radio.text}", Toast.LENGTH_LONG).show()
+
+
+
+        }
+
+
+
     }
 }
