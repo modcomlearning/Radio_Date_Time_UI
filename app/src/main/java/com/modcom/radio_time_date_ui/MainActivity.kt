a78eb9a3e77cfb3a2807c2ce2c2263ff1429d44c
modcomlearning <modcomlearning@gmail.com>
2021-04-08 10:17:31 +0300

    Second Commit

--- app/src/main/java/com/modcom/radio_time_date_ui/MainActivity.kt
+++ app/src/main/java/com/modcom/radio_time_date_ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,25 @@ class MainActivity : AppCompatActivity() {
             //how to capture the names
             val names = edit_names.text.toString()
 
+            //how to capture time/date
+            //ill be on audio shortly
+            //first we get todays date
+            val today = Calendar.getInstance()
+            date.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH))
+            {
+                view, year, month, day ->
+                val month = month + 1
+                Toast.makeText(applicationContext, "You selected  Date $day/$month/$year ", Toast.LENGTH_LONG).show()
+
+            }
+
+
 
             Toast.makeText(applicationContext, "You selected  ${radio.text}  Your Names $names", Toast.LENGTH_LONG).show()
 
-        }
+        }//ends button listener
 
 
 
-    }
+    }//end oncreate
 }
